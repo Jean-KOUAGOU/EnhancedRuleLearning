6190f413eaf6028e7753464eb169582e36304e1d
Jean-KOUAGOU <jeank@aims.ac.za>
2022-10-11 16:34:01 +0200

    updated documentation

--- Method/link_prediction/core.py
+++ Method/link_prediction/core.py
@@ -3,6 +3,9 @@ import torch
 import random
 import warnings
 import json
+import os
+
+base_path = os.path.dirname(os.path.realpath(__file__)).split('link_prediction')[0]
 
 from tqdm import tqdm
 
@@ -10,23 +13,23 @@ class LinkPredictor:
     """Python class implementing link prediction from pretrained embeddings"""
     
     def __init__(self, kwargs):
-        self.model = kwargs.model_name
+        self.model = kwargs.model
+        self.kg = (kwargs.kg).lower()
         self.target_relations = kwargs.target_relations
-        self.entity_emb_path = kwargs.entity_emb_path
-        self.relation_emb_path = kwargs.relation_emb_path
-        self.full_embedding_path = kwargs.full_embedding_path
+        self.full_embedding_path = base_path+'embeddings/'+self.kg+'_'+self.model+'/trained_model.pkl'
         
         self.max_num_entities = kwargs.max_num_entities
+        self.max_num_relations = kwargs.max_num_relations
         self.min_score = kwargs.min_score
         
-        self.path_to_triples = kwargs.path_to_triples
+        self.path_to_triples = base_path+f'datasets/{self.kg}/train.txt'
         self.device = ("cuda" if torch.cuda.is_available() else "cpu")
         
-        self.entity_id_map = kwargs.entity_id_map
-        self.relation_id_map = kwargs.relation_id_map
-        kg = open(self.path_to_triples, 'r')
-        kg = kg.readlines()
-        self.kg = {tp: i for i,tp in enumerate(kg)} # dictionary for fast lookup
+        self.entity_id_map = base_path+'embeddings/'+self.kg+'_'+self.model+'/entity_to_ids.json'
+        self.relation_id_map = base_path+'embeddings/'+self.kg+'_'+self.model+'/relation_to_ids.json'
+        triples = open(self.path_to_triples, 'r')
+        triples = triples.readlines()
+        self.triples = {tp: i for i,tp in enumerate(triples)} # dictionary for fast lookup
         self.seed = kwargs.random_seed
         self.store_triples = kwargs.store_triples
         
@@ -34,7 +37,7 @@ class LinkPredictor:
     def load_embeddings(self):
         model = torch.load(self.full_embedding_path, map_location=self.device)
         with open(self.entity_id_map) as file:
-            entity_id_map = json.load(file )
+            entity_id_map = json.load(file)
         with open(self.relation_id_map) as file:
             relation_id_map = json.load(file)
         ent_emb = pd.DataFrame(model.entity_embeddings._embeddings.weight.data.tolist(), index=list(entity_id_map.keys()))
@@ -47,25 +50,28 @@ class LinkPredictor:
         print('\n############################## Finding new triples ##############################\n')
         self.load_embeddings()
         
+        random.seed(self.seed)
         triples = []
         if self.target_relations == []:
             rels = list(self.relation_embs.index)
+            rels = random.sample(rels, min(len(rels),self.max_num_relations))
+            print(f"Using the following relations: {rels}")
         else:
             rels = list(self.relation_embs.index)
             rels = [r for r in rels if any([p.lower() in r.lower() for p in self.target_relations])]
             print("Target relations: {}".format(rels))
         ents = list(self.entity_embs.index)
         
-        random.seed(self.seed)
         random.shuffle(ents)
         
-        group1 = ents[:self.max_num_entities]
-        group2 = ents[self.max_num_entities : 2*self.max_num_entities]
+        max_num_ent = min(self.max_num_entities, len(ents)//2)
+        group1 = ents[:max_num_ent]
+        group2 = ents[max_num_ent : 2*max_num_ent]
         
         for rel in tqdm(rels, desc='Outer loop (on relations)'):
             for e1 in group1:
                 for e2 in group2:
-                    if not e1+'\t'+rel+'\t'+e2+'\n' in self.kg:
+                    if not e1+'\t'+rel+'\t'+e2+'\n' in self.triples:
                         triples.append([e1, rel, e2])
         print('\n*********************** Number of initial candidate triples: {} ***********************\n'.format(len(triples)))
         return triples
@@ -78,8 +84,8 @@ class LinkPredictor:
     
         def Distmult_pykeen(h,r,t):
             if h.ndim == 1:
-                return 2*torch.sigmoid(-torch.norm((h*r)-t, p=2))
-            return 2*torch.sigmoid(-torch.norm((h*r)-t, p=2, dim=-1))
+                return torch.sigmoid(torch.norm((h*r)*t, p=1))
+            return torch.sigmoid(torch.norm((h*r)*t, p=1, dim=-1))
         if self.model == 'TransE':
             return TransE_pykeen(emb_e1, emb_r, emb_e2)
         if self.model == 'Distmult':
@@ -98,6 +104,8 @@ class LinkPredictor:
             r_s = torch.tensor(self.relation_embs.loc[new_triples[:, 1].tolist()].values)
             e2_s = torch.tensor(self.entity_embs.loc[new_triples[:, 2].tolist()].values)
             scores = self.score(e1_s, r_s, e2_s)
+            print('All scores: ', scores)
+            print('Max score: ', max(scores))
             valid_indices = (scores >= self.min_score).tolist()
             suggested_triples = new_triples[valid_indices].tolist()
             self.suggested_triples = suggested_triples
@@ -111,10 +119,11 @@ class LinkPredictor:
             print('\n'+'*'*20)
             print('Storing new triples')
             print('*'*20)
-            write_out_path1 = self.path_to_triples.split('train')[0]+'train_completed.txt'
-            write_out_path2 = self.path_to_triples.split('train')[0]+'new_triples.txt'
+            write_out_path1 = self.path_to_triples.split('train')[0]+f'train_completed_{self.model}.txt'
+            write_out_path2 = self.path_to_triples.split('train')[0]+f'new_triples_{self.model}.txt'
             with open(write_out_path1, 'w') as file1:
-                file1.writelines(list(self.kg.keys()))
+                file1.writelines(list(self.triples.keys()))
                 file1.writelines(list(map(lambda x: x[0]+'\t'+x[1]+'\t'+x[2]+'\n', self.suggested_triples)))
             with open(write_out_path2, 'w') as file2:
                 file2.writelines(list(map(lambda x: x[0]+'\t'+x[1]+'\t'+x[2]+'\n', self.suggested_triples)))
+            print('Done !')
--- README.md
+++ README.md
@@ -21,50 +21,53 @@ An error may occur during installation of pykeen. In such a case, use ```pip ins
 Install Java (version 8 or higher) in the environment (secondment): ```conda install -c bioconda java-jdk```
 
 
-Download ```datasets.zip``` from [drive](https://drive.google.com/file/d/1yrHSw4FZTYpkI3Qs8y2ejW1slH51zYJF/view?usp=sharing), extract it into Method/
+Download ```datasets.zip``` from [drive](https://drive.google.com/file/d/1qu2_c_SCBql6QGFA6hipiIlgw0p4fKRr/view?usp=sharing), extract them into Method/
+
+Download pretrained embeddings ` embeddings.zip ` from [drive](https://drive.google.com/file/d/1JKlP61JqJDMwh7rPnwjAQmBQbfSuHGQR/view?usp=sharing) and extract them into Method/
 
 
-## Reproducing the reported results
+## Reproduce the reported results on improved rule mining with AMIE+:
 
-### Link Prediction
+- Make sure Jupyter lab is installed. Open the file ` rule_mining.ipynb ` and run all cells.
+
+
+
+## New triple inference
 
 *Open a terminal and navigate into Method/* ``` cd EnhancedRuleLearning/Method/```
+
 - Predict new links on DRKG: ``` python predict_new_links ``` with the following options:
 
 ``` 
   --model_name MODEL_NAME
                         The embedding model used, e.g. TransE
-  --full_embedding_path FULL_EMBEDDING_PATH
-                        Full embedding path, expecting a pykeen embedding
-                        model saved as a pickle object
+                        
+  --kg KG               The knowledge graph name
+  
   --target_relations TARGET_RELATIONS [TARGET_RELATIONS ...]
                         Relation names to use for link prediction
-  --entity_emb_path ENTITY_EMB_PATH
-                        Entity embedding path
-  --relation_emb_path RELATION_EMB_PATH
-                        Relation embedding path
+                        
   --max_num_entities MAX_NUM_ENTITIES
                         The maximum number of entities to select
+                        
   --min_score MIN_SCORE
-                        The minimum score triples should have to be accepted
-                        as true triples
+                        The minimum score triples should have to be accepted as true triples
+                        
   --random_seed RANDOM_SEED
-                        Random seed for random entity selection for link
-                        prediction
-  --path_to_triples PATH_TO_TRIPLES
-                        Path to data triples
-  --entity_id_map ENTITY_ID_MAP
-                        Entity name to id map, same as the one used during
-                        training
-  --relation_id_map RELATION_ID_MAP
-                        Relation name to id map, same as the one used during
-                        training
-  --store_triples STORE_TRIPLES
+                        Random seed for random entity selection for link prediction
+                        
+  --store_triples [STORE_TRIPLES]
                         Whether to store the newly predicted triples
 ```
 
-*Navigate into Method/anyBURL/* ``` cd Link_Prediction_NCES/Method/anyBURL```
+## Running AMIE+:
 
-*Configure config-learn.properties and*:
+Copy ` amie-dev.jar ` into ` Method/AMIE ` and ` Method/TransAMIE `
 
-- Learn AnyBURL rules: ```java -Xmx4G -cp anyBURL-RE.jar de.unima.ki.anyburl.LearnReinforced config-learn.properties```
+- Run AMIE+ on original knowledge graphs: ` cd Method/AMIE ` then ` java -jar amie-dev.jar ../datasets/{kb}/train.txt `
+
+- Run AMIE+ on completed knowledge graphs: `cd Method/TransAMIE ` then ` java -jar amie-dev.jar ../datasets/{kb}/train_completed_TransE.txt `
+
+- To save the generated rules in each case, specify an output directory and file name by adding ` >> directory_name/file_name.txt ` in front of the lines above
+
+
