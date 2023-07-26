050837890baf50bf852a4a41a47394f5e595cafd
Jean-KOUAGOU <jeank@aims.ac.za>
2023-07-26 17:46:38 +0200

    update scripts

--- Method/link_prediction/core.py
+++ Method/link_prediction/core.py
@@ -1,5 +1,6 @@
 import pandas as pd, numpy as np
 import torch
+#import pykeen.nn.representation
 import random
 import warnings
 import json
@@ -12,38 +13,35 @@ from tqdm import tqdm
 class LinkPredictor:
     """Python class implementing link prediction from pretrained embeddings"""
     
-    def __init__(self, kwargs):
-        self.model = kwargs.model
-        self.kg = (kwargs.kg).lower()
+    def __init__(self, kg, model, kwargs):
+        self.model_name = model.lower()
+        self.kg = kg.lower()
         self.target_relations = kwargs.target_relations
-        self.full_embedding_path = base_path+'embeddings/'+self.kg+'_'+self.model+'/trained_model.pkl'
+        self.full_embedding_path = base_path+'embeddings/'+self.kg+'_'+self.model_name+'/trained_model.pkl'
         
-        self.max_num_entities = kwargs.max_num_entities
-        self.max_num_relations = kwargs.max_num_relations
-        self.min_score = kwargs.min_score
+        self.num_entities = kwargs.num_entities
+        self.num_relations = kwargs.num_relations
+        self.topk_triples = kwargs.topk_triples
         
         self.path_to_triples = base_path+f'datasets/{self.kg}/train.txt'
         self.device = ("cuda" if torch.cuda.is_available() else "cpu")
         
-        self.entity_id_map = base_path+'embeddings/'+self.kg+'_'+self.model+'/entity_to_ids.json'
-        self.relation_id_map = base_path+'embeddings/'+self.kg+'_'+self.model+'/relation_to_ids.json'
-        triples = open(self.path_to_triples, 'r')
-        triples = triples.readlines()
-        self.triples = {tp: i for i,tp in enumerate(triples)} # dictionary for fast lookup
+        self.entity_id_map = base_path+'embeddings/'+self.kg+'_'+self.model_name+'/entity_to_ids.json'
+        self.relation_id_map = base_path+'embeddings/'+self.kg+'_'+self.model_name+'/relation_to_ids.json'
+        with open(self.path_to_triples, 'r') as file:
+            triples = file.readlines()
+        self.triples = {tp: i for i,tp in enumerate(triples)} # use a dictionary for fast lookup
         self.seed = kwargs.random_seed
         self.store_triples = kwargs.store_triples
         
         
     def load_embeddings(self):
-        model = torch.load(self.full_embedding_path, map_location=self.device)
+        self.model = torch.load(self.full_embedding_path, map_location=self.device)
+        self.model.eval()
         with open(self.entity_id_map) as file:
-            entity_id_map = json.load(file)
+            self.entity_id_map = json.load(file)
         with open(self.relation_id_map) as file:
-            relation_id_map = json.load(file)
-        ent_emb = pd.DataFrame(model.entity_embeddings._embeddings.weight.data.tolist(), index=list(entity_id_map.keys()))
-        rel_emb = pd.DataFrame(model.relation_embeddings._embeddings.weight.data.tolist(), index=list(relation_id_map.keys()))
-        self.entity_embs = ent_emb
-        self.relation_embs = rel_emb
+            self.relation_id_map = json.load(file)
     
     def get_new_triples(self):
         
@@ -53,18 +51,23 @@ class LinkPredictor:
         random.seed(self.seed)
         triples = []
         if self.target_relations == []:
-            rels = list(self.relation_embs.index)
-            rels = random.sample(rels, min(len(rels),self.max_num_relations))
+            if self.kg == 'drkg':
+                rels = ['DGIDB::INHIBITOR::Gene:Compound', 'DRUGBANK::treats::Compound:Disease', 'GNBR::C::Compound:Disease',
+                        'GNBR::Pa::Compound:Disease', 'GNBR::Pr::Compound:Disease', 'GNBR::T::Compound:Disease',
+                        'Hetionet::CpD::Compound:Disease', 'Hetionet::CtD::Compound:Disease']
+            else:
+                rels = list(self.relation_id_map.keys())
+                rels = random.sample(rels, min(len(rels),self.num_relations))
             print(f"Using the following relations: {rels}")
         else:
-            rels = list(self.relation_embs.index)
-            rels = [r for r in rels if any([p.lower() in r.lower() for p in self.target_relations])]
+            rels = list(self.relation_id_map.keys())
+            rels = [r for r in rels if any([t.lower() in r.lower() for t in self.target_relations])]
             print("Target relations: {}".format(rels))
-        ents = list(self.entity_embs.index)
+        ents = list(self.entity_id_map)
         
         random.shuffle(ents)
         
-        max_num_ent = min(self.max_num_entities, len(ents)//2)
+        max_num_ent = min(self.num_entities, len(ents)//2)
         group1 = ents[:max_num_ent]
         group2 = ents[max_num_ent : 2*max_num_ent]
         
@@ -73,57 +76,39 @@ class LinkPredictor:
                 for e2 in group2:
                     if not e1+'\t'+rel+'\t'+e2+'\n' in self.triples:
                         triples.append([e1, rel, e2])
+                    #if not e2+'\t'+rel+'\t'+e1+'\n' in self.triples:
+                    #    triples.append([e2, rel, e1])
         print('\n*********************** Number of initial candidate triples: {} ***********************\n'.format(len(triples)))
         return triples
-      
-    def score(self, emb_e1, emb_r, emb_e2):
-        def TransE_pykeen(h, r, t):
-            if h.ndim == 1:
-                return 2(1-torch.sigmoid(-torch.norm(h+r-t, p=2)))
-            return 2*(1-torch.sigmoid(-torch.norm(h+r-t, p=2, dim=-1)))
-    
-        def Distmult_pykeen(h,r,t):
-            if h.ndim == 1:
-                return torch.sigmoid(torch.norm((h*r)*t, p=1))
-            return torch.sigmoid(torch.norm((h*r)*t, p=1, dim=-1))
-        if self.model == 'TransE':
-            return TransE_pykeen(emb_e1, emb_r, emb_e2)
-        if self.model == 'Distmult':
-            return Distmult_pykeen(emb_e1, emb_r, emb_e2)
-        raise ValueError
     
     def get_suggested_triples(self):
         new_triples = self.get_new_triples()
         new_triples = np.array(new_triples)
-        suggested_triples = []
+        self.suggested_triples = []
         
         print('\n############################## Getting highest scoring triples from Pykeen {} embeddings ##############################\n'\
-             .format(self.model))
-        try:
-            e1_s = torch.tensor(self.entity_embs.loc[new_triples[:, 0].tolist()].values)
-            r_s = torch.tensor(self.relation_embs.loc[new_triples[:, 1].tolist()].values)
-            e2_s = torch.tensor(self.entity_embs.loc[new_triples[:, 2].tolist()].values)
-            scores = self.score(e1_s, r_s, e2_s)
-            print('All scores: ', scores)
-            print('Max score: ', max(scores))
-            valid_indices = (scores >= self.min_score).tolist()
-            suggested_triples = new_triples[valid_indices].tolist()
-            self.suggested_triples = suggested_triples
-        except IndexError:
-            print("Empty array %s" % suggested_triples)
-        print('\n*********************** Number of suggested triples with score >= {}: {} ***********************\n'.format(self.min_score, len(suggested_triples)))
+             .format(self.model_name))
+        #try:
+        e1_s = torch.tensor(list(map(self.entity_id_map.get, new_triples[:, 0]))).unsqueeze(1)
+        r_s = torch.tensor(list(map(self.relation_id_map.get, new_triples[:, 1]))).unsqueeze(1)
+        e2_s = torch.tensor(list(map(self.entity_id_map.get, new_triples[:, 2]))).unsqueeze(1)
+        h_r_t = torch.cat([e1_s,r_s,e2_s], dim=1)
+        scores = self.model.score_hrt(h_r_t)
+        sorted_vals, indices = torch.sort(scores.detach().squeeze(), descending=True) # sort scores and rank triples
+        valid_indices = indices.tolist()[:self.topk_triples]
+        self.suggested_triples = new_triples[valid_indices]
         return self
     
     def write_out_suggested_triples(self):
         if self.store_triples is True:
             print('\n'+'*'*20)
-            print('Storing new triples')
+            print('\n*********************** Storing top {} suggested triples ***********************\n'.format(len(self.suggested_triples)))
             print('*'*20)
-            write_out_path1 = self.path_to_triples.split('train')[0]+f'train_completed_{self.model}.txt'
-            write_out_path2 = self.path_to_triples.split('train')[0]+f'new_triples_{self.model}.txt'
+            write_out_path1 = self.path_to_triples.split('train')[0]+f'train_completed_{self.model_name}_top{self.topk_triples}.txt'
+            write_out_path2 = self.path_to_triples.split('train')[0]+f'new_triples_{self.model_name}_top{self.topk_triples}.txt'
             with open(write_out_path1, 'w') as file1:
                 file1.writelines(list(self.triples.keys()))
                 file1.writelines(list(map(lambda x: x[0]+'\t'+x[1]+'\t'+x[2]+'\n', self.suggested_triples)))
             with open(write_out_path2, 'w') as file2:
                 file2.writelines(list(map(lambda x: x[0]+'\t'+x[1]+'\t'+x[2]+'\n', self.suggested_triples)))
-            print('Done !')
+        print('Done !')
--- Method/predict_new_links.py
+++ Method/predict_new_links.py
@@ -12,13 +12,15 @@ if __name__ == "__main__":
         else:
             raise ValueError('Ivalid boolean value.')
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', type=str, default='TransE', help="The embedding model used, e.g. TransE")
-    parser.add_argument('--kg', type=str, default='drkg', help="The knowledge graph name")
-    parser.add_argument('--target_relations', nargs='+', default=[], help="Relation names to use for link prediction")
-    parser.add_argument('--max_num_entities', type=int, default=100, help="The maximum number of entities to select")
-    parser.add_argument('--min_score', type=float, default=0.8, help="The minimum score triples should have to be accepted as true triples")
+    parser.add_argument('--models', type=str, nargs='+', default=['transe'], choices=['transe', 'distmult', 'rotate'], help="The embedding model used, e.g. TransE")
+    parser.add_argument('--kgs', type=str, nargs='+', default=['drkg'], help="The knowledge graph name")
+    parser.add_argument('--target_relations', type=str, nargs='+', default=[], help="Relation names to use for link prediction")
+    parser.add_argument('--num_entities', type=int, default=1000, help="The number of entities to select")
     parser.add_argument('--random_seed', type=int, default=142, help="Random seed for random entity selection for link prediction")
-    parser.add_argument('--max_num_relations', type=int, default=10, help="The maximum number of relations to use in case none was given")
-    parser.add_argument('--store_triples', type=str2bool, const=True, default=True, nargs='?', help="Whether to store the newly predicted triples")
+    parser.add_argument('--num_relations', type=int, default=10, help="The maximum number of relations to use in case none was given")
+    parser.add_argument('--topk_triples', type=int, default=50, help="Top 50 highest scoring triples")
+    parser.add_argument('--store_triples', type=str2bool, const=True, default=False, nargs='?', help="Whether to store the newly predicted triples")
     args = parser.parse_args()
-    link_predictor = LinkPredictor(args).get_suggested_triples().write_out_suggested_triples()
+    for kg in args.kgs:
+        for model in args.models:
+            link_predictor = LinkPredictor(kg, model, args).get_suggested_triples().write_out_suggested_triples()
